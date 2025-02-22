063bc04806b303ffc7df4b8075933095c56d7124
Naman Mittal <namanmittal501@gmail.com>
2025-02-22 23:04:54 +0530

    Testing_mocha_chai

---
+++ test/index.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const app = require('../index');
+
+
+describe('GET /', () => {
+    it('responds with "Hello, World!"', (ho_gaya) => {
+        request(app).get('/').expect(200,'Hello, World!', ho_gaya);
+    });
+});
+
+
+
+describe("POST/user", () => {
+    it("should create a user with valid test", (done) => {
+      request(app)
+        .post("/user")
+        .send({ name: "Coder29" })
+        .expect(201, "User Coder29 created", done);
+    });
+
+
+it("should return 400 if name is missing", (done) => {
+    request(app).post("/user").send({}).expect(400, "Name is required", done);
+  });
+});

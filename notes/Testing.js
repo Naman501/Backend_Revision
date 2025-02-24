c51a7da5ce3d5649c5d4cb7ffb5cc52ade06e79a
Naman Mittal <namanmittal501@gmail.com>
2025-02-24 14:07:50 +0530

    Mocha_chai

---
+++ notes/Testing.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app= express();
+const port=5000
+app.use(express.json())
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello, World!")
+})
+
+app.post("/user",(req,res)=>{
+    const {name}=req.body;
+    if(!name){
+      return  res.status(400).send("Name is required")}
+
+    res.status(201).send(`User ${name} created`)
+})
+
+module.exports=app
